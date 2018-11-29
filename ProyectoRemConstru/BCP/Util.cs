80295b5685f50ccdb127f9d5c6668fd5a0716756
Jorge Bascuñán <kokeshiet@gmail.com>
2018-11-28 23:45:02 -0300

    Se agreag Clase Util con metodo calcularDV en biblioteca BCP

---
+++ ProyectoRemConstru/BCP/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCP
+{
+    class Util
+    {
+        public static string calculaDV(int rut)
+        {
+            int suma = 0;
+            int multiplicador = 1;
+            while (rut != 0)
+            {
+                multiplicador++;
+                if (multiplicador == 8)
+                    multiplicador = 2;
+                suma += (rut % 10) * multiplicador;
+                rut = rut / 10;
+            }
+            suma = 11 - (suma % 11);
+            if (suma == 11)
+            {
+                return "0";
+            }
+            else if (suma == 10)
+            {
+                return "K";
+            }
+            else
+            {
+                return suma.ToString();
+            }
+        }
+    }
+}
